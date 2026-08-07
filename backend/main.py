5105ebbfb25b0005daadfc41726eb58fe0b8d5f3
Tri Hung <2200006625@nttu.edu.vn>
2026-08-07 19:51:07 +0700

    fix: update cors and dynamic port for railway

--- backend/main.py
+++ backend/main.py
@@ -19,10 +19,11 @@ app = FastAPI(title="ShopHub Product API với PostgreSQL", version="2.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["*"],
 )
 
 Base.metadata.create_all(bind=engine)
@@ -42,4 +43,5 @@ def read_root():
     return {"status": "success", "message": "Backend ShopHub chạy PostgreSQL ngon lành!"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
